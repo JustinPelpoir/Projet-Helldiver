8284d8547c9028cab1ae8ac68bf4a5dd1ad6fd3d
JustinPelpoir <justin.pelpoir@efrei.net>
2025-05-29 22:26:31 +0200

    Added function to start quiz by hiding previous inscription box

---
+++ js/form.js
@@ -0,0 +1,18 @@
+function quizAlert() {
+
+  // Message d’alerte
+  alert("Merci pour votre inscription. Le quiz démocratique va commencer...");
+
+  // Cacher le formulaire d’inscription
+  document.getElementById('informations').style.display = 'none';
+
+}
+
+
+
+
+
+  function submitQuiz() {
+    alert("Merci pour vos réponses ! Vive la démocratie encadrée !");
+    // Tu peux ajouter ici un traitement ou une redirection
+  }
